899daf8715780798ee49ac78a40252dd72eb0e90
Adeleke <lekea4@gmail.com>
2021-03-18 16:25:25 +0100

    Updated date for work resumption

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-const countDownDate = new Date("April 1, 2021 15:37:25").getTime();
+const countDownDate = new Date("April 5, 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
 const x = setInterval(function() {
@@ -11,10 +11,10 @@ const x = setInterval(function() {
   const distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
@@ -23,6 +23,6 @@ const x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "CONGRATULATIONS ON YOUR NEW JOB";
+    document.getElementById("demo").innerHTML = "CONGRATULATIONS ON YOUR NEW JOB TOOCHUKWU OBASI";
   }
 }, 1000);
